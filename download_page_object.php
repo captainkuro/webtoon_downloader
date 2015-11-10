c9bb67f62511bee0be23f4a71029f92eaacce9ea
Khandar William <khandar.william@gdplabs.id>
2015-11-10 11:54:18 +0700

    change language to IN

--- download_page_object.php
+++ download_page_object.php
@@ -8,6 +8,7 @@ class WebtoonScraper {
 				'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
 				'Accept-Language: en-US,en;q=0.8'
 			),
+			CURLOPT_COOKIE => 'contentLanguage=id;locale=in',
 			CURLOPT_FOLLOWLOCATION => 1,
 		);
 		$this->cli = new Display_CLI();
--- download_procedural.php
+++ download_procedural.php
@@ -20,6 +20,7 @@ $opts = array(
 		'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
 		'Accept-Language: en-US,en;q=0.8'
 	),
+	CURLOPT_COOKIE => 'contentLanguage=id;locale=in',
 	CURLOPT_FOLLOWLOCATION => 1,
 );
 
